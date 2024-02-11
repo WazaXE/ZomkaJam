a51f0b2e85b4048e0f04692fd053166b587a9aea
017-Fredrik <107189080+FreDZOMKA@users.noreply.github.com>
2024-02-11 12:28:20 +0100

    q3

--- Proto_Player/PlayerRaw.gd
+++ Proto_Player/PlayerRaw.gd
@@ -10,6 +10,6 @@ func _physics_process(_delta: float):
 	
 	 
 func  _unhandled_input(_event: InputEvent):
-	dir.x = Input.get_axis("ui_left", "ui_right")
-	dir.y = Input.get_axis("ui_up", "ui_down")
+	dir.x = Input.get_axis("move_left", "move_right")
+	dir.y = Input.get_axis("move_up", "move_down")
 	dir = dir.normalized()
